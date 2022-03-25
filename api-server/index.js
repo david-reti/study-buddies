a75b49cac71a75b383f83468c015c4c5f78315b4
David Reti <dreti@uoguelph.ca>
2022-03-25 03:03:14 +0000

    Implement routes for scheduling

--- api-server/index.js
+++ api-server/index.js
@@ -1,9 +1,32 @@
+const Bookshelf = require('./bookshelf');
 const express = require('express');
+const scheduling = require('./scheduling');
+
 const app = express();
-const port = 3000;
+app.use(express.json());
+
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
+});
+
+app.post('/user/:id/scheduled-timeslots', async (req, res) => {
+    let scheduleData = req.body;
+    if(!scheduleData.courseID || !scheduleData.timeslot) {
+        return res.json({'message': `Could not schedule timeslot because of missing parameters`});
+    }
+
+    res.json(await scheduling.schedule_user(req.params.id, scheduleData.courseID, scheduleData.timeslot));
+});
+
+app.get('/user/:id/scheduled-timeslots', async (req, res) => {
+    if(await Bookshelf.BookShelf.model('ScheduledTimeslot').where({'userID': req.params.id}).count() > 0) {
+        let timeslots = await Bookshelf.BookShelf.model('ScheduledTimeslot').where({'userID': req.params.id}).fetchAll();
+        res.json({ "message": "OK", "timeslots": timeslots});
+    } else {
+        res.json({ "message": "OK", "timeslots": {}});
+    }
 });
 
 app.listen(port, () => {
---
+++ api-server/models.js
@@ -0,0 +1,50 @@
+const Bookshelf = require('./bookshelf');
+
+// This file is where all the data models (e.g. the database) can be defined
+// The top part has some functions for generating tables. Each function tables a "table" object, and then you can
+// Add different types of columns to it
+//
+// The bottom part has the bookshelf models, which should match the table and will be used to query it.
+// Whenever you run regenerateTables.js, it will create the tables from the functions defined here
+
+// About Models: You register a model by calling Bookshelf.Bookshelf.model with the name of the model to register, passing in parameters
+// When you call Bookshelf.Bookshelf.model without passing in params, it will instead retrieve the models (which is what we do in different files)
+
+function createCourse(table) {
+    table.increments();
+    table.text('fullname');
+    table.text('coursecode');
+    table.text('department');
+}
+
+Bookshelf.BookShelf.model('Course', {
+    tablename: 'course'
+});
+
+function createUser(table) {
+    table.increments();
+    table.text('email', 256).notNullable();
+    table.text('password', 64).notNullable();
+}
+
+Bookshelf.BookShelf.model('User', {
+    tableName: 'user'
+});
+
+function createScheduledTimeslot(table) {
+    table.increments('id');
+    table.integer('userID');
+    table.text('courseID');
+    table.string('timeslot');
+}
+
+Bookshelf.BookShelf.model('ScheduledTimeslot', {
+    tableName: 'scheduledtimeslot',
+    userID() {
+        return this.belongsTo('User', 'userid', 'id');
+    }
+});
+
+exports.createUser = createUser;
+exports.createCourse = createCourse;
+exports.createScheduledTimeslot = createScheduledTimeslot;
